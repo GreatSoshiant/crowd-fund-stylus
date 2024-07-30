e156c4fb437b262cfd038b35ccce0f755316349f
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2024-07-30 12:04:13 +0300

    small changes

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,10 @@ extern crate alloc;
 use std::borrow::BorrowMut;
 
 use alloy_primitives::{Address, U256};
-use alloy_sol_types::sol;
 use stylus_sdk::{
-    block,
-    call::Call,
-    contract,
-    evm::{self},
-    msg,
-    prelude::*,
+    block, call::Call, contract, evm::{self}, msg, prelude::*
 };
+use alloy_sol_types::sol;
 
 sol_interface! {
     interface IERC20 {
@@ -20,7 +15,7 @@ sol_interface! {
     }
 }
 
-sol! {
+sol!{
     event Launch(
         uint256 id,
         address indexed creator,
@@ -35,14 +30,14 @@ sol! {
     event Refund(uint256 id, address indexed caller, uint256 amount);
 }
 
+
 sol_storage! {
-    #[entrypoint]
     pub struct CrowdFund{
     // Total count of campaigns created.
     // It is also used to generate id for new campaigns.
     uint256 count;
     // The address of the NFT contract.
-    address token_address;
+    address token_address; 
     // Mapping from id to Campaign
     CampaignStruct[] campaigns; // The transactions array
     // Mapping from campaign id => pledger => amount pledged
@@ -69,13 +64,15 @@ sol_storage! {
 #[external]
 impl CrowdFund {
     pub const ONE_DAY: u64 = 86400; // 1 day = 24 hours * 60 minutes * 60 seconds = 86400 seconds.
+
+
     pub fn launch(&mut self, goal: U256, start_at: U256, end_at: U256) {
-        assert!(start_at < U256::from(block::timestamp()));
-        assert!(end_at < start_at);
+        assert!(start_at<U256::from(block::timestamp()));
+        assert!(end_at<start_at);
         assert!(end_at > U256::from(block::timestamp() + 7 * Self::ONE_DAY));
 
         let number = self.count.get();
-        self.count.set(number + U256::from(1));
+        self.count.set(number+ U256::from(1));
 
         let mut new_campaign = self.campaigns.grow();
         new_campaign.creator.set(msg::sender());
@@ -90,61 +87,56 @@ impl CrowdFund {
             creator: msg::sender(),
             goal: goal,
             start_at: start_at,
-            end_at: end_at,
+            end_at:end_at
         });
     }
     pub fn cancel(&mut self, id: U256) {
         if let Some(mut entry) = self.campaigns.get_mut(id) {
             if entry.creator.get() == msg::sender()
-                && U256::from(block::timestamp()) > entry.start_at.get()
-            {
-                entry.creator.set(Address::ZERO);
-                entry.goal.set(U256::from(0));
-                entry.pledged.set(U256::from(0));
-                entry.start_at.set(U256::from(0));
-                entry.end_at.set(U256::from(0));
-                entry.claimed.set(false);
-                evm::log(Cancel { id: id });
+             && U256::from(block::timestamp()) > entry.start_at.get() {
+            entry.creator.set(Address::ZERO);
+            entry.goal.set(U256::from(0));
+            entry.pledged.set(U256::from(0));
+            entry.start_at.set(U256::from(0));
+            entry.end_at.set(U256::from(0));
+            entry.claimed.set(false);
+            evm::log(Cancel{id:id});
             }
         }
     }
-    pub fn pledge<S: TopLevelStorage + BorrowMut<Self>>(storage: &mut S, id: U256, amount: U256) {
+    pub fn pledge<S: TopLevelStorage + BorrowMut<Self>>(storage: &mut S, id: U256, amount: U256)  {
         if let Some(mut entry) = storage.borrow_mut().campaigns.get_mut(id) {
             if U256::from(block::timestamp()) >= entry.start_at.get()
-                && U256::from(block::timestamp()) <= entry.end_at.get()
-            {
-                let pledged = U256::from(entry.pledged.get());
-                entry.pledged.set(pledged + amount);
-                let mut pledged_amount_info = storage.borrow_mut().pledged_amount.setter(id);
-                let mut pledged_amount_sender = pledged_amount_info.setter(msg::sender());
-                let old_amount = pledged_amount_sender.get();
-                pledged_amount_sender.set(old_amount + amount);
+             && U256::from(block::timestamp()) <= entry.end_at.get() {
+                    let pledged = U256::from(entry.pledged.get());
+                    entry.pledged.set(pledged + amount);
+                    let mut pledged_amount_info = storage.borrow_mut().pledged_amount.setter(id);
+                    let mut pledged_amount_sender = pledged_amount_info.setter(msg::sender());
+                    let old_amount = pledged_amount_sender.get();
+                    pledged_amount_sender.set(old_amount + amount);
 
-                let token = IERC20::new(*storage.borrow_mut().token_address);
-                let config = Call::new_in(storage);
-                token.transfer(config, contract::address(), amount).unwrap();
+                    let token = IERC20::new(*storage.borrow_mut().token_address);
+                    let config = Call::new_in(storage);
+                    token.transfer(config, contract::address(), amount).unwrap();
             }
-        }
+         }
+
     }
     pub fn unpledge<S: TopLevelStorage + BorrowMut<Self>>(storage: &mut S, id: U256, amount: U256) {
         if let Some(mut entry) = storage.borrow_mut().campaigns.get_mut(id) {
-            if U256::from(block::timestamp()) <= entry.end_at.get() {
-                let pledged = U256::from(entry.pledged.get());
-                entry.pledged.set(pledged - amount);
-                let mut pledged_amount_info = storage.borrow_mut().pledged_amount.setter(id);
-                let mut pledged_amount_sender = pledged_amount_info.setter(msg::sender());
-                let old_amount = pledged_amount_sender.get();
-                pledged_amount_sender.set(old_amount - amount);
-                // Token transfer
-                let token = IERC20::new(*storage.borrow_mut().token_address);
-                let config = Call::new_in(storage);
-                token.transfer(config, contract::address(), amount).unwrap();
-                // Emit the log
-                evm::log(Unpledge {
-                    id: id,
-                    caller: msg::sender(),
-                    amount: amount,
-                });
+            if U256::from(block::timestamp()) <= entry.end_at.get(){
+                    let pledged = U256::from(entry.pledged.get());
+                    entry.pledged.set(pledged - amount);
+                    let mut pledged_amount_info = storage.borrow_mut().pledged_amount.setter(id);
+                    let mut pledged_amount_sender = pledged_amount_info.setter(msg::sender());
+                    let old_amount = pledged_amount_sender.get();
+                    pledged_amount_sender.set(old_amount - amount);
+                    // Token transfer
+                    let token = IERC20::new(*storage.borrow_mut().token_address);
+                    let config = Call::new_in(storage);
+                    token.transfer(config, contract::address(), amount).unwrap();
+                    // Emit the log
+                    evm::log(Unpledge {id: id, caller: msg::sender(), amount: amount});
             }
         }
     }
@@ -156,7 +148,7 @@ impl CrowdFund {
             let pledged = entry.pledged.get();
             let goal = entry.goal.get();
             let claimed = entry.claimed.get();
-
+    
             // Check conditions on the entry
             if creator == msg::sender()
                 && U256::from(block::timestamp()) > end_at
@@ -165,14 +157,14 @@ impl CrowdFund {
             {
                 // Mark the entry as claimed
                 entry.claimed.set(true);
-
+    
                 // Now, perform the token transfer
                 let token_address = *storage.borrow_mut().token_address;
                 let token = IERC20::new(token_address);
-
+    
                 let config = Call::new_in(storage);
                 token.transfer(config, creator, pledged).unwrap();
-                evm::log(Claim { id: id });
+                evm::log(Claim{id:id});
             }
         }
     }
@@ -184,22 +176,20 @@ impl CrowdFund {
             let goal = entry.goal.get();
             let pledged = entry.pledged.get();
 
-            if U256::from(block::timestamp()) > end_at && pledged < goal {
+            if U256::from(block::timestamp()) > end_at
+            && pledged < goal {
                 let mut pledged_amount_info = storage.borrow_mut().pledged_amount.setter(id);
                 let mut pledged_amount_sender = pledged_amount_info.setter(msg::sender());
                 let old_balance = pledged_amount_sender.get();
                 pledged_amount_sender.set(U256::from(0));
                 let token_address = *storage.borrow_mut().token_address;
                 let token = IERC20::new(token_address);
-
+    
                 let config = Call::new_in(storage);
                 token.transfer(config, msg::sender(), old_balance).unwrap();
-                evm::log(Refund {
-                    id: id,
-                    caller: msg::sender(),
-                    amount: old_balance,
-                });
+                evm::log(Refund{id: id, caller: msg::sender(), amount: old_balance});
+           
             }
         }
-    }
+    }   
 }
